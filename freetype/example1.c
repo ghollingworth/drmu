58f3b9c00ca6fcdebc7058b72dfecd06118bdbdc
John Cox <jc@kynesim.co.uk>
2024-03-14 17:21:03 +0000

    freetype: Build & use runticker

--- freetype/example1.c
+++ freetype/example1.c
@@ -2,36 +2,14 @@
  * screen
  */
 
-#include <errno.h>
-#include <fcntl.h>
-#include <poll.h>
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 
-#include <sys/eventfd.h>
+#include <drmu.h>
+#include <drmu_log.h>
+#include <drmu_output.h>
+#include <drmu_scan.h>
 
-#include <ft2build.h>
-#include FT_FREETYPE_H
-
-#include "drmu.h"
-#include "drmu_log.h"
-#include "drmu_output.h"
-#include "drmu_scan.h"
-
-#include <drm_fourcc.h>
-
-#include "ticker.h"
-
-#define WIDTH   640
-#define HEIGHT  128
-
-typedef struct dft_env_s {
-    ticker_env_t *te;
-    const char *text;
-    const char *cchar;
-    int prod_fd;
-} dft_env_t;
+#include "runticker.h"
 
 static void
 drmu_log_stderr_cb(void *v, enum drmu_log_level_e level, const char *fmt, va_list vl)
@@ -48,34 +26,14 @@ drmu_log_stderr_cb(void *v, enum drmu_log_level_e level, const char *fmt, va_lis
     fwrite(buf, n + 1, 1, stderr);
 }
 
-static int
-next_char_cb(void *v)
-{
-    dft_env_t *const dfte = v;
-
-    if (*dfte->cchar == 0)
-        return -1;
-    return *dfte->cchar++;
-}
-
-static void
-do_prod(void *v)
-{
-    static const uint64_t one = 1;
-    dft_env_t *const dfte = v;
-    write(dfte->prod_fd, &one, sizeof(one));
-}
-
 int
 main(int argc, char **argv)
 {
-    dft_env_t *dfte = calloc(1, sizeof(*dfte));
     drmu_env_t *du;
     drmu_output_t *dout;
-    int trv;
+    runticker_env_t * rte;
 
     const char *const device = NULL;
-    const char *filename;
 
     static const drmu_log_env_t log = {
         .fn = drmu_log_stderr_cb,
@@ -88,75 +46,20 @@ main(int argc, char **argv)
         exit(1);
     }
 
-    if (dfte == NULL) {
-        fprintf(stderr, "No memory for env\n");
-        return 1;
-    }
-
-    filename      = argv[1];                           /* first argument     */
-    dfte->text    = argv[2];                           /* second argument    */
-    dfte->cchar = dfte->text;
-
     if (drmu_scan_output(device, &log, &du, &dout) != 0) {
         fprintf(stderr, "Failed drmu scan for device\n");
         return 1;
     }
     drmu_env_restore_enable(du);
 
-    if ((dfte->te = ticker_new(dout, 0, 128, 720, 128)) == NULL) {
+    if ((rte = runticker_start(dout, 0, 128, 720, 128, argv[2], argv[1])) == NULL) {
         fprintf(stderr, "Failed to create ticker\n");
         return 1;
     }
     drmu_output_unref(&dout); // Ticker keeps a ref
 
-    if (ticker_set_face(dfte->te, filename) != 0) {
-        fprintf(stderr, "Failed to set face\n");
-        return 1;
-    }
+    getchar();
 
-    ticker_next_char_cb_set(dfte->te, next_char_cb, dfte);
-
-    if ((dfte->prod_fd = eventfd(0, EFD_NONBLOCK)) == -1) {
-        fprintf(stderr, "Failed to get event fd\n");
-        return 1;
-    }
-    ticker_commit_cb_set(dfte->te, do_prod, dfte);
-
-    if (ticker_init(dfte->te) != 0) {
-        fprintf(stderr, "Failed to init ticker\n");
-        return 1;
-    }
-
-    while ((trv = ticker_run(dfte->te)) >= 0) {
-        char evt_buf[8];
-        int rv;
-
-        struct pollfd pf[2] = {
-            { .fd = dfte->prod_fd, .events = POLLIN, .revents = 0 },
-            { .fd = STDIN_FILENO, .events = POLLIN, .revents = 0 },
-        };
-
-        while ((rv = poll(pf, 2, -1)) < 0 && errno == EINTR)
-            /* loop */;
-
-        /* 0 = timeout but that should never happen */
-        if (rv <= 0) {
-            fprintf(stderr, "Poll failure\n");
-            break;
-        }
-
-        /* Keypress */
-        if (pf[1].revents != 0)
-            break;
-
-        read(dfte->prod_fd, evt_buf, 8);
-    }
-
-    if (trv < 0)
-        getchar();
-
-    ticker_delete(&dfte->te);
-    close(dfte->prod_fd);
-    free(dfte);
+    runticker_stop(&rte);
     return 0;
 }
--- freetype/runticker.c
+++ freetype/runticker.c
@@ -1,47 +1,146 @@
 #include "runticker.h"
 
+#include <errno.h>
+#include <poll.h>
 #include <pthread.h>
 #include <string.h>
-#include <stdio.h>
+#include <stdatomic.h>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <unistd.h>
 
+#include <sys/eventfd.h>
+
+#include <drmu.h>
+#include <drmu_log.h>
 #include <drmu_output.h>
 
-typedef struct ticker_env_s {
-    struct drmu_output_s * dout;
-    unsigned int x;
-    unsigned int y;
-    unsigned int w;
-    unsigned int h;
-} ticker_env_t;
+#include "ticker.h"
+
+struct runticker_env_s {
+    atomic_int kill;
+    ticker_env_t *te;
+    char *text;
+    const char *cchar;
+    int prod_fd;
+    bool thread_running;
+    pthread_t thread_id;
+};
+
+static int
+next_char_cb(void *v)
+{
+    runticker_env_t *const dfte = v;
+
+    if (*dfte->cchar == 0)
+        dfte->cchar = dfte->text;
+    return *dfte->cchar++;
+}
+
+static void
+do_prod(void *v)
+{
+    static const uint64_t one = 1;
+    runticker_env_t *const dfte = v;
+    write(dfte->prod_fd, &one, sizeof(one));
+}
 
 static void *
-cube_thread(void * v)
+runticker_thread(void * v)
 {
-    ticker_env_t * const te = v;
+    runticker_env_t * const dfte = v;
 
-    drmu_output_unref(&te->dout);
-    free(te);
+    while (!atomic_load(&dfte->kill) && ticker_run(dfte->te) >= 0) {
+        char evt_buf[8];
+        int rv;
+
+        struct pollfd pf[1] = {
+            { .fd = dfte->prod_fd, .events = POLLIN, .revents = 0 },
+        };
+
+        while ((rv = poll(pf, 1, -1)) < 0 && errno == EINTR)
+            /* loop */;
+
+        /* 0 = timeout but that should never happen */
+        if (rv <= 0)
+            break;
+
+        read(dfte->prod_fd, evt_buf, 8);
+    }
+
     return NULL;
 }
 
-int
-runticker_start(struct drmu_output_s * dout, unsigned int x, unsigned int t, unsigned int w, unsigned int h)
+runticker_env_t *
+runticker_start(drmu_output_t * const dout,
+                unsigned int x, unsigned int y, unsigned int w, unsigned int h,
+                const char * const text,
+                const char * const fontfile)
 {
-    pthread_t thread_id;
-    ticker_env_t * te = calloc(1, sizeof(*te));
+    runticker_env_t *dfte = calloc(1, sizeof(*dfte));
+    drmu_env_t * const du = drmu_output_env(dout);
 
-    if (te == NULL)
-        return -1;
+    if (dfte == NULL)
+        return NULL;
 
-    te->dout = drmu_output_ref(dout);
-    te->x = x;
-    te->y = y;
-    te->w = w;
-    te->h = h;
+    dfte->prod_fd = -1;
+    dfte->text  = strdup(text);
+    dfte->cchar = dfte->text;
 
+    if ((dfte->te = ticker_new(dout, x, y, w, h)) == NULL) {
+        drmu_err(du, "Failed to create ticker");
+        goto fail;
+    }
 
-    pthread_create(&thread_id, NULL, ticker_thread, te);
-    return 0;
+    if (ticker_set_face(dfte->te, fontfile) != 0) {
+        drmu_err(du, "Failed to set face\n");
+        goto fail;
+    }
+
+    ticker_next_char_cb_set(dfte->te, next_char_cb, dfte);
+
+    if ((dfte->prod_fd = eventfd(0, EFD_NONBLOCK)) == -1) {
+        drmu_err(du, "Failed to get event fd");
+        goto fail;
+    }
+    ticker_commit_cb_set(dfte->te, do_prod, dfte);
+
+    if (ticker_init(dfte->te) != 0) {
+        drmu_err(du, "Failed to init ticker");
+        goto fail;
+    }
+
+    if (pthread_create(&dfte->thread_id, NULL, runticker_thread, dfte) != 0) {
+        drmu_err(du, "Failed to create thread");
+        goto fail;
+    }
+    dfte->thread_running = true;
+
+    return dfte;
+
+fail:
+    runticker_stop(&dfte);
+    return NULL;
+}
+
+void
+runticker_stop(runticker_env_t ** const ppDfte)
+{
+    runticker_env_t * const dfte = *ppDfte;
+    if (dfte == NULL)
+        return;
+    *ppDfte = NULL;
+
+    if (dfte->thread_running) {
+        atomic_store(&dfte->kill, 1);
+        do_prod(dfte);
+        pthread_join(dfte->thread_id, NULL);
+    }
+
+    ticker_delete(&dfte->te);
+    if (dfte->prod_fd != -1)
+        close(dfte->prod_fd);
+    free(dfte->text);
+    free(dfte);
 }
 
